9d99b7134114e39ddfbb18125743ac3800be26f1
Hidetake Iwata <int128@gmail.com>
2017-11-16 20:24:10 +0900

    Refactor

--- src/main/kotlin/org/hidetake/gradleupdate/app/IndexController.kt
+++ src/main/kotlin/org/hidetake/gradleupdate/app/IndexController.kt
@@ -1,13 +1,28 @@
 package org.hidetake.gradleupdate.app
 
 import org.springframework.stereotype.Controller
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.servlet.ModelAndView
+import javax.validation.constraints.NotNull
 
 @Controller
-@RequestMapping("/")
 class IndexController {
-    @GetMapping
-    fun get() = ModelAndView("index")
+    @GetMapping("/")
+    fun index() = ModelAndView("index")
+
+    @PostMapping("/landing")
+    fun landing(@Validated repositoryForm: RepositoryForm, bindingResult: BindingResult) =
+        when {
+            bindingResult.hasErrors() -> "/"
+            else -> repositoryForm.extractOwnerRepo()?.let { "$it/status" } ?: "/"
+        }.let { uri ->
+            "redirect:$uri"
+        }
+
+    data class RepositoryForm(@NotNull var url: String = "") {
+        fun extractOwnerRepo(): String? = Regex("""[^/]+/[^/]+$""").find(url)?.value
+    }
 }
